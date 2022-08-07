3ee641f891d4f9a76cf2f6b3f58655f914b31002
Alexander Oliveira <acc01719@sky.com.br>
2022-08-07 12:35:23 -0300

    inclindo a lista de funcionarios da empresa

---
+++ src/company/myEmployees.js
@@ -0,0 +1,65 @@
+var listMyEmployees =
+[{
+  "id": 1,
+  "name": "Alex",
+  "last_name": "Jose",
+  "office": "Operador de máquina",
+  "data_actual": "3 meses"
+}, {
+  "id": 2,
+  "name": "Maria",
+  "last_name": "Josefa",
+  "office": "Auxiliar de produção",
+  "data_actual": "4 meses"
+}, {
+  "id": 3,
+  "name": "Reinaldo",
+  "last_name": "do Reis",
+  "office": "Faxineiro",
+  "data_actual": "5 meses"
+}, {
+  "id": 4,
+  "name": "Maria",
+  "last_name": "Ferreira",
+  "office": "Cozinheira",
+  "data_actual": "6 meses"
+}, {
+  "id": 5,
+  "name": "Danilo",
+  "last_name": "Oliveira",
+  "office": "Auxiliar de amoxerifado",
+  "data_actual": "7 meses"
+}, {
+  "id": 6,
+  "name": "Renata",
+  "last_name": "Madrid",
+  "office": "Operadora de máquina II",
+  "data_actual": "8 meses"
+}, {
+  "id": 7,
+  "name": "Jesse",
+  "last_name": "Ataide",
+  "office": "Auxiliar de manuntenção",
+  "data_actual": "3 meses"
+}, {
+  "id": 8,
+  "name": "Sara",
+  "last_name": "Oliveira",
+  "office": "Cozinheira",
+  "data_actual": "1 Mês"
+}, {
+  "id": 9,
+  "name": "Tairone",
+  "last_name": "da silva",
+  "office": "Porteiro",
+  "data_actual": "11 meses"
+}, {
+  "id": 10,
+  "name": "Joana",
+  "last_name": "Rodrigues",
+  "office": "Auxiliar de produção",
+  "data_actual": "1 ano"
+}]
+
+module.exports = listMyEmployees;
+
