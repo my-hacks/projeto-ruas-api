c813434f98a3a43ecd2780c23b42468914720fc7
Emerson Silva <emerson_jdss@hotmail.com>
2022-08-07 13:41:20 -0300

    adjust dates

--- src/ong/referencsJobs.js
+++ src/ong/referencsJobs.js
@@ -2,21 +2,21 @@ var listReferencesJobs =
   [{
     "id": 1,
     "office": "Ajudante geral",
-    "expiration_date": "1/16/2021",
+    "expiration_date": "16/01/2021",
     "status": false,
     "description": "Trabalho em ambientes para limpeza",
     "company": 'Carrefour'
   }, {
     "id": 2,
     "office": "Auxiliar de cozinha",
-    "expiration_date": "2/17/2022",
+    "expiration_date": "17/02/2022",
     "status": false,
     "description": "Auxiliar chefe de cozinha",
     "company": 'Carrefour'
   }, {
     "id": 3,
     "office": "Office boy",
-    "expiration_date": "3/14/2022",
+    "expiration_date": "14/03/2022",
     "status": false,
     "description": "Entrega de documentos e analise documental escrituária",
     "company": 'Carrefour'
@@ -30,34 +30,34 @@ var listReferencesJobs =
   }, {
     "id": 5,
     "office": "Auxiliar de amoxerifado",
-    "expiration_date": "5/25/2021",
+    "expiration_date": "25/05/2021",
     "status": true,
     "description": "Entrega de documentos e analise documental escrituária",
     "company": 'Queiroz Galvão'
   }, {
     "id": 6,
     "office": "Servente de pedreiro",
-    "expiration_date": "06/23/2021",
+    "expiration_date": "23/06/2021",
     "status": true,
     "description": "Auxílio aos pedreiros e mestres de obra em geral",
     "company": 'Odebretch'
   }, {
     "id": 7,
     "office": "Pedreiro",
-    "expiration_date": "07/26/2021",
+    "expiration_date": "26/07/2021",
     "status": true,
     "description": "Refomas, Ampliações e Manutenções Prediais. Manutenção preventiva e corretiva",
   }, {
     "id": 8,
     "office": "Jardineiro",
-    "expiration_date": "08/25/2021",
+    "expiration_date": "25/08/2021",
     "status": false,
     "description": "Realiza implantação, criação e manutenção de jardins",
     "company": 'Odebretch'
   }, {
     "id": 9,
     "office": "Porteiro",
-    "expiration_date": "09/18/2022",
+    "expiration_date": "18/09/2022",
     "status": true,
     "description": "Atendimento ao publico, controlar a entrada e saída de pessoas",
     "company": 'Extra Hipermercados'
